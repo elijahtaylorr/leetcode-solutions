00bd1ca7378f386b5a3619217a5d63a0b1c77b99
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-10-30 14:01:36 +0800

    Time: 136 ms (32.20%) | Memory: 13.5 MB (35.59%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -7,34 +7,11 @@ class Solution(object):
         rows = len(mat)
         columns = len(mat[0])
         special = 0
-    
+
         for rowNum in range(rows):
             for colNum in range(columns):
                 if mat[rowNum][colNum] == 1:
                     if sum(mat[rowNum]) == 1 and sum(row[colNum] for row in mat) == 1:
                         special += 1
-    
-        return special
 
-        """
-        :type mat: List[List[int]]
-        :rtype: int
-        """
-        columns = len(mat)
-        rows = len(mat[0])
-        special = 0
-        for colNum in range(rows):
-            isSpecial = False
-            for rowNum in range(columns):
-                cell = mat[rowNum][colNum]
-                if cell == 1 and not isSpecial:
-                    isSpecial = True
-                elif cell == 1 and isSpecial:
-                    isSpecial = False
-                    break
-            if isSpecial:
-                for checkColumns in range(columns):
-                    colCell = mat[rowNum][checkColumns]
-                special+= 1
         return special
-
