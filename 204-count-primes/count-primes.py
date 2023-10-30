521ba956637fb914207d36e2ada69f13ee77531c
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-10-30 15:05:18 +0800

    Time: 2355 ms (74.96%) | Memory: 131.2 MB (82.04%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def countPrimes(self, n):
-        if n <= 2: # If n is 2 or less, there are no prime numbers less than n.
+        if n <= 2:
             return 0
-        sieve = [True] * n  # The list will now have indices from 0 to n-1
-        sieve[0], sieve[1] = False, False  # 0 and 1 are not primes
+        sieve = [True] * n
+        sieve[0], sieve[1] = False, False
         p = 2
-        while (p * p < n):  # You want primes less than n
+        while (p * p < n):
             if sieve[p]:
                 for i in range(p * p, n, p):
                     sieve[i] = False
