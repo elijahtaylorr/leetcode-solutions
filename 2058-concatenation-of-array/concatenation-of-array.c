d076b718cbee9515828d174da89a9b9a290c2ed5
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-02 13:49:23 +0800

    Time: 30 ms (57.89%) | Memory: 10.6 MB (15.55%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.c
+++ 2058-concatenation-of-array/concatenation-of-array.c
@@ -4,11 +4,9 @@
 int* getConcatenation(int* nums, int numsSize, int* returnSize){
     int *rtn = malloc(2 * numsSize * sizeof(int));
     for(size_t i = 0;i < numsSize;i++){
-        printf("1. Setting %d to %d\n",i,nums[i]);
         rtn[i] = nums[i];
     }
     for(int i = numsSize;i < 2*numsSize;i++){
-        printf("2. Setting %d to %d\n",i,nums[i-numsSize]);
         rtn[i] = nums[i-numsSize];
     }
     *returnSize = (2*numsSize);
