f8ebfb6281006b5213825434e293d74c82a4225e
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-03 18:43:53 +0800

    Time: 94 ms (13.75%) | Memory: 13 MB (77.50%) - LeetSync

--- 890-lemonade-change/lemonade-change.c
+++ 890-lemonade-change/lemonade-change.c
@@ -1,14 +1,13 @@
 bool lemonadeChange(int* bills, int billsSize) {
-    int *billCount = calloc(3,sizeof(int));
+    int *billCount = calloc(2,sizeof(int));
     if(bills[0] > 5){
-        printf("failing on start iteration");
         return false;
     }
     for(size_t i = 0; i < billsSize; i++){
         if(bills[i] == 5){
             billCount[0]++;
         }else if(bills[i] == 10){
-            if(billCount[0] < 1){printf("failing on %d iteration",i); return false;}
+            if(billCount[0] < 1){return false;}
             billCount[0]-= 1;
             billCount[1]++;
         } else {
@@ -17,16 +16,13 @@ bool lemonadeChange(int* bills, int billsSize) {
                     billCount[0]--;
                     billCount[1]--;
                 }else{
-                    printf("failing on %d iteration",i);
                     return false;
                 }
             }else if(billCount[0] >= 3){
                 billCount[0]-= 3;
             } else {
-                printf("failing on %d iteration",i);
                 return false;
             }
-            billCount[2]++;
         }
     }
     return true;
