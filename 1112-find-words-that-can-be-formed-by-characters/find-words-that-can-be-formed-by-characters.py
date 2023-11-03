daf24172305d6b257354d23c0992268d9b78a2ca
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-03 13:29:15 +0800

    Time: 264 ms (29.30%) | Memory: 14.2 MB (20.93%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -2,18 +2,16 @@ class Solution(object):
     def countCharacters(self, words, chars):
         countChrs = Counter(chars)
         goodWords = []
+        
         for word in words:
+            wordCounter = Counter(word)
             wordGood = True
-            for letter in word:
-                if (countChrs[letter] <= 0):
+            for letter in wordCounter:
+                if wordCounter[letter] > countChrs[letter]:
                     wordGood = False
-                countChrs[letter]-= 1
+                    break
             if wordGood:
                 goodWords.append(word)
-            countChrs = Counter(chars)
-        finalSum = 0
-        for goodWord in goodWords:
-            finalSum+= len(goodWord)
+        
+        finalSum = sum(len(goodWord) for goodWord in goodWords)
         return finalSum
-
-                
