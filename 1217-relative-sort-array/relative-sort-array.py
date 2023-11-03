a6198794dfe6c7f0af835a18b7be0d42d1e77565
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-03 13:19:58 +0800

    Time: 19 ms (72.89%) | Memory: 13.3 MB (45.78%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -2,11 +2,9 @@ class Solution(object):
     def relativeSortArray(self, arr1, arr2):
         numMap = Counter(arr1)
         rtnList = []
-        lastInd = 0
         for val in arr2:
             for i in range(numMap[val]):
                 rtnList.append(val)
-                lastInd+= 1
         otherVals = []
         for val in arr1:
             if val not in rtnList:
