e77f84a367daf5d604c9381631391aee9a625906
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-04 11:53:12 +0800

    Time: 3 ms (47.39%) | Memory: 6.5 MB (15.17%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.c
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.c
@@ -1,9 +1,8 @@
 int kthFactor(int n, int k) {
-    int kCounter = 0;
     for(size_t i = 1; i <= n; i++){
         if(n % i == 0){
-            kCounter++;
-            if(kCounter == k){
+            k--;
+            if(k == 0){
                 return i;
             }
         }
