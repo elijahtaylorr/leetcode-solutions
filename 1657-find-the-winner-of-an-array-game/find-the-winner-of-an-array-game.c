369a8d0571b9941647b80bd3a1f54077db7d8bb1
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-05 13:48:37 +0800

    Time: 75 ms (55.56%) | Memory: 13.5 MB (33.33%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.c
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.c
@@ -2,6 +2,21 @@ int getWinner(int* arr, int arrSize, int k) {
     int currentWinner = arr[0];
     int winCount = 0;
 
+    if(k >= arrSize){
+        int maxVal = 0;
+        for(size_t i = 0; i < arrSize; i++){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }else if(k == 1){
+        if(arr[0] > arr[1]){
+            return arr[0];
+        }
+        return arr[1];
+    }
+
     for (int i = 1; i < arrSize; i++) {
         if (arr[i] > currentWinner) {
             currentWinner = arr[i];
