9f4c1712b15af8297ac7aaa3b9272bc4c3bb590e
Elijah Taylor <45134707+elijahtaylorr@users.noreply.github.com>
2023-11-06 11:22:06 +0800

    Time: 1835 ms (20.00%) | Memory: 110.3 MB (73.33%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.c
+++ 1955-seat-reservation-manager/seat-reservation-manager.c
@@ -1,41 +1,37 @@
-
-
-
 typedef struct {
     bool* reserved;
     int reservationsSize;
     int nextSeat;
 } SeatManager;
 
-
 SeatManager* seatManagerCreate(int n) {
     SeatManager* seats = malloc(sizeof(SeatManager));
-    seats->reserved = calloc(n,sizeof(int));
-    seats->reservationsSize = n;
-    seats->nextSeat = 0;
+    seats->reserved = calloc(n, sizeof(bool));
+    *seats = (SeatManager){.reserved = seats->reserved, .reservationsSize = n, .nextSeat = 0};
     return seats;
 }
 
 int seatManagerReserve(SeatManager* obj) {
-    while (obj->nextSeat < obj->reservationsSize && obj->reserved[obj->nextSeat]){
-        obj->nextSeat++;
+    if (obj->nextSeat == obj->reservationsSize) {
+        return -1;
     }
 
-    if(obj->nextSeat < obj->reservationsSize){
-        obj->reserved[obj->nextSeat] = true;
-        return obj->nextSeat+1;
-    }
-    return 1;
+    obj->reserved[obj->nextSeat] = true;
+    int reservedSeat = obj->nextSeat;
+
+    do {
+        obj->nextSeat++;
+    } while (obj->nextSeat < obj->reservationsSize && obj->reserved[obj->nextSeat]);
+
+    return reservedSeat + 1;
 }
 
 void seatManagerUnreserve(SeatManager* obj, int seatNumber) {
-    if (seatNumber > 0 && seatNumber <= obj->reservationsSize){
-        int seatIdx = seatNumber-1;
-        if(obj->reserved[seatIdx]){
-            obj->reserved[seatIdx] = false;
-            if(seatIdx < obj->nextSeat){
-                obj->nextSeat = seatIdx;
-            }
+    int seatIdx = seatNumber - 1;
+    if (seatIdx >= 0 && seatIdx < obj->reservationsSize) {
+        obj->reserved[seatIdx] = false;
+        if (seatIdx < obj->nextSeat) {
+            obj->nextSeat = seatIdx;
         }
     }
 }
@@ -44,13 +40,3 @@ void seatManagerFree(SeatManager* obj) {
     free(obj->reserved);
     free(obj);
 }
-
-/**
- * Your SeatManager struct will be instantiated and called as such:
- * SeatManager* obj = seatManagerCreate(n);
- * int param_1 = seatManagerReserve(obj);
- 
- * seatManagerUnreserve(obj, seatNumber);
- 
- * seatManagerFree(obj);
-*/
